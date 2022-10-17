07a73bbce8cb61358d00b42716fdca1f800d4cb0
quanhspdz <pc.quanhdz@gmail.com>
2022-10-17 15:23:15 +0700

    design main activity toolbar

--- app/src/main/java/com/example/lightning/activities/MainActivity.java
+++ app/src/main/java/com/example/lightning/activities/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.lightning.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.lightning.R;
 
@@ -16,8 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setStatusBarColor();
 
+    }
 
+    private void setStatusBarColor() {
+        Window window = this.getWindow();
 
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.blue_toolbar));
     }
 }
