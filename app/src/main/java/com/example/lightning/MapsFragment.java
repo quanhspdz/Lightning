deb371b123ced427152172676c2fa84274d90f5b
quanhspdz <pc.quanhdz@gmail.com>
2022-10-23 10:46:49 +0700

    load lasted trip to SearchForDriverActivity

---
+++ app/src/main/java/com/example/lightning/MapsFragment.java
@@ -0,0 +1,6 @@
+package com.example.lightning;
+
+import android.app.Activity;
+
+public class MapsFragment extends Activity {
+}
--- app/src/main/java/com/example/lightning/activities/SearchForDriverActivity.java
+++ app/src/main/java/com/example/lightning/activities/SearchForDriverActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.lightning.R;
@@ -15,13 +19,26 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class SearchForDriverActivity extends AppCompatActivity implements OnMapReadyCallback {
-    
+
+    EditText edtPickUp, edtDes;
+    ImageView imgVehicle;
+    TextView textVehicleType, textTimeCost, textMoneyCost;
+
+    public final int maxLength = 34;
+
     private String tripId;
     private Trip trip;
     GoogleMap map;
@@ -33,10 +50,34 @@ public class SearchForDriverActivity extends AppCompatActivity implements OnMapR
         setContentView(R.layout.activity_search_for_driver);
 
         init();
+    }
 
+    private void setTripInfo() {
+        edtDes.setText(trip.getDropOffName());
+        edtPickUp.setText(trip.getPickUpName());
+        textTimeCost.setText(trip.getTimeCost());
+        textMoneyCost.setText(trip.getCost());
+
+        if (trip.getVehicleType().equals(Const.car)) {
+            imgVehicle.setImageResource(R.drawable.car);
+            textVehicleType.setText("Car");
+        } else {
+            imgVehicle.setImageResource(R.drawable.shipper);
+            textVehicleType.setText("Motorbike");
+        }
     }
 
     private void init() {
+        edtDes = findViewById(R.id.edt_dropOfPos);
+        edtPickUp = findViewById(R.id.edt_pickUpPos);
+        textMoneyCost = findViewById(R.id.text_moneyCost);
+        textTimeCost = findViewById(R.id.text_timeCost);
+        textVehicleType = findViewById(R.id.text_vehicleType);
+        imgVehicle = findViewById(R.id.imgVehicle);
+
+        edtDes.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxLength)});
+        edtPickUp.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxLength)});
+
         UET = new LatLng(21.038902482537342, 105.78296809797327); //Dai hoc Cong Nghe Lat Lng
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fragment_maps);
@@ -49,11 +90,37 @@ public class SearchForDriverActivity extends AppCompatActivity implements OnMapR
         
         Intent intent = getIntent();
         if (intent.getStringExtra("tripId") == null) {
-            Toast.makeText(this, "Can not get tripId!", Toast.LENGTH_SHORT).show();
+           getLastedTrip();
         } else {
             tripId = intent.getStringExtra("tripId");
             getTripInfo(tripId);
         }
+    }
+
+    private void getLastedTrip() {
+        List<Trip> listTrips = new ArrayList<>();
+        FirebaseDatabase.getInstance().getReference().child("Trips")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            Trip trip = dataSnapshot.getValue(Trip.class);
+                            if (trip != null)
+                                if (trip.getPassengerId().equals(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())) {
+                                    listTrips.add(trip);
+                                }
+                        }
+                        if (listTrips.size() > 0) {
+                            trip = listTrips.get(listTrips.size() - 1);
+                            setTripInfo();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 
     private void getTripInfo(String tripId) {
@@ -66,6 +133,7 @@ public class SearchForDriverActivity extends AppCompatActivity implements OnMapR
                         if (trip != null) {
                             updateTripStatus(Const.searching, trip);
                         }
+                        setTripInfo();
                     }
 
                     @Override
