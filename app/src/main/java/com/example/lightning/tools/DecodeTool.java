8bd1dfc90bcee900c97d27c08f801b39df0dbe68
quanhspdz <pc.quanhdz@gmail.com>
2022-11-02 09:47:04 +0700

    create method to calculate distance between 2 LatLng

--- app/src/main/java/com/example/lightning/tools/DecodeTool.java
+++ app/src/main/java/com/example/lightning/tools/DecodeTool.java
@@ -1,5 +1,7 @@
 package com.example.lightning.tools;
 
+import android.location.Location;
+
 import com.google.android.gms.maps.model.LatLng;
 
 public class DecodeTool {
@@ -12,5 +14,17 @@ public class DecodeTool {
         latLng = new LatLng(Double.parseDouble(arrOfStr[0]), Double.parseDouble(arrOfStr[1]));
 
         return latLng;
+    }
+
+    public static double calculateDistance(LatLng origin, LatLng dest) {
+        Location locationOri = new Location("LocationA");
+        locationOri.setLatitude(origin.latitude);
+        locationOri.setLongitude(origin.longitude);
+
+        Location locationDest = new Location("LocationA");
+        locationDest.setLatitude(dest.latitude);
+        locationDest.setLongitude(dest.longitude);
+
+        return locationOri.distanceTo(locationDest);
     }
 }
