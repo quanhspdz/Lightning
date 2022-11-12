ef84e68e1f6b82f5a1fedf737d40ce8ba13555e3
quanhspdz <pc.quanhdz@gmail.com>
2022-11-12 13:26:15 +0700

    load passenger profile image into circle image view in main-act

--- app/src/main/java/com/example/lightning/activities/MainActivity.java
+++ app/src/main/java/com/example/lightning/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lightning.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
@@ -14,12 +15,24 @@ import android.widget.EditText;
 import android.widget.FrameLayout;
 
 import com.example.lightning.R;
+import com.example.lightning.models.Passenger;
 import com.example.lightning.tools.Const;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtSearchPlace;
     FrameLayout btnYourLocation, btnHistory, btnCar, btnMotor, btnVoucher, btnSettings;
+    CircleImageView imageProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         setStatusBarColor();
         init();
+        getPassengerInfo();
         listener();
 
     }
@@ -88,6 +102,30 @@ public class MainActivity extends AppCompatActivity {
         btnMotor = findViewById(R.id.buttonMotor);
         btnVoucher = findViewById(R.id.buttonVoucher);
         btnSettings = findViewById(R.id.buttonSettings);
+        imageProfile = findViewById(R.id.img_profile);
+    }
+
+    private void getPassengerInfo() {
+        FirebaseDatabase.getInstance().getReference()
+                .child("Passengers")
+                .child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        Passenger passenger = snapshot.getValue(Passenger.class);
+                        if (passenger != null) {
+                            Picasso.get().load(passenger.getPassengerImageUrl())
+                                    .resize(1000, 1000)
+                                    .centerCrop()
+                                    .into(imageProfile);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 
     private void setStatusBarColor() {
