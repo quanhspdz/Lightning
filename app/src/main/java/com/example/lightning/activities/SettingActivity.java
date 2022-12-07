27cc6d14d6588748b1f42ad550134afea509b3d7
quanhspdz <pc.quanhdz@gmail.com>
2022-12-07 13:54:36 +0700

    design setting-act

---
+++ app/src/main/java/com/example/lightning/activities/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.example.lightning.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lightning.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
